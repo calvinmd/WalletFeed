bcb7a4a6c4f683a2b537a023f46bd04ed34f405e
Shain <srslafazan@gmail.com>
2018-10-05 22:07:17 -0700

    Styled top menu buttons

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import cx from 'classnames'
 import * as log from 'loglevel'
 import { MdChevronLeft } from 'react-icons/md'
 import { FaUser } from 'react-icons/fa'
@@ -7,10 +8,28 @@ import { FaUser } from 'react-icons/fa'
 import './Header.sass';
 
 
+class MenuTopBarButton extends Component {
+  render() {
+    return (
+      <div
+        className={cx({
+          MenuTopBarButton: true,
+          active: this.props.active,
+        })}
+        onClick={this.props.onClick || (() => {})}>
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
 class MenuTopBar extends Component {
   render() {
     return (
       <div className='MenuTopBar'>
+        <MenuTopBarButton active={false} onClick={() => console.log('All')}>All</MenuTopBarButton>
+        <MenuTopBarButton active={true} onClick={() => console.log('Watchlist')}>Watchlist</MenuTopBarButton>
+        <MenuTopBarButton active={false} onClick={() => console.log('Me')}>Me</MenuTopBarButton>
       </div>
     );
   }
