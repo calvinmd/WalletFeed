62fe2f00f20e48e093af92a650135274762e9012
Shain <srslafazan@gmail.com>
2018-10-05 22:23:40 -0700

    Added footer buttons

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,24 +1,42 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import * as log from 'loglevel'
+import { FaCoins, FaPuzzlePiece, FaGamepad } from 'react-icons/fa'
+import { MdSwapHoriz, MdSettings, MdMoreHoriz } from 'react-icons/md'
 
 import './Footer.sass';
 
 
-// class MenuButton extends Component {
-//   render() {
-//     return (
-//       <div className='MenuButton'>
-//       </div>
-//     );
-//   }
-// }
+class FooterMenuButton extends Component {
+  render() {
+    return (
+      <div className='FooterMenuButton'>
+        {this.props.children}
+      </div>
+    );
+  }
+}
 
 
 class Footer extends Component {
   render() {
     return (
       <footer className="Footer">
+        <FooterMenuButton onClick={() => console.log('onclick')}>
+          <FaCoins />
+        </FooterMenuButton>
+        <FooterMenuButton onClick={() => console.log('onclick')}>
+          <FaGamepad />
+        </FooterMenuButton>
+        <FooterMenuButton onClick={() => console.log('onclick')}>
+          <MdSwapHoriz />
+        </FooterMenuButton>
+        <FooterMenuButton onClick={() => console.log('onclick')}>
+          <MdSettings />
+        </FooterMenuButton>
+        <FooterMenuButton onClick={() => console.log('onclick')}>
+          <MdMoreHoriz />
+        </FooterMenuButton>
       </footer>
     );
   }
