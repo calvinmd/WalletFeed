4e7089206463a270f641e9478db5ba8fd4e18af8
calvinmd <calvinpak@gmail.com>
2018-10-06 18:44:51 -0700

    remove unused package

--- server/routes/api/v1/transfers.js
+++ server/routes/api/v1/transfers.js
@@ -1,5 +1,4 @@
 const router = require('express').Router();
-const mapSeries = require('async/mapSeries');
 const logger = require('@/constructors/logger');
 const axios = require('axios');
 const url = require('url');
